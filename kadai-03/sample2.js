f6b368882a8f92642689338a4eb36edac829d2ce
Kamata Ryo <6292312+kamataryo@users.noreply.github.com>
2021-11-09 08:12:40 +0000

    課題1,3,4を追加

--- kadai-03/sample2.js
+++ kadai-03/sample2.js
@@ -1,3 +1,10 @@
-for (let index = 0; index < 100; index = index + 1) {
-    console.log(index)
+for (let i = 5; i < 10; i = i + 1) {
+    console.log(i)
+}
+for (let n = 1; n < 101; n = n + 1) {
+    if (n === 50) {
+        continue
+    } else {
+        console.log(n)
+    }
 }
---
+++ kadai-04/sample02.js
@@ -0,0 +1,11 @@
+function greet(name) {
+    console.log("こんにちは、" + name + "さん！")
+}
+function add(a, b) {
+    console.log(a + b)
+}
+
+greet("山田")
+greet("太郎")
+add(100, 101)
+add("山田", "太郎")
